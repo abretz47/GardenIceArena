968da3228f283ca47d743e58f9d3a22ddcdbbc70
pglow11 <pglow0503@gmail.com>
2024-03-09 11:13:50 -0600

    Reverting Program

--- src/Web/Web.Server/Program.cs
+++ src/Web/Web.Server/Program.cs
@@ -5,7 +5,23 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-RegisterServices(builder);
+var keyVault = builder.Configuration.GetValue<string>("KeyVault") ?? throw new InvalidOperationException();
+var managedIdentityClientId = builder.Configuration.GetValue<string>("ManagedIdentityClientId") ?? throw new InvalidOperationException();
+var client = new SecretClient(new Uri(keyVault), new DefaultAzureCredential(new DefaultAzureCredentialOptions { ManagedIdentityClientId = managedIdentityClientId }));
+var connectionString = client.GetSecret("ApplicationDbContextConnection").Value.Value ?? throw new InvalidOperationException();
+
+builder.Services.AddDbContext<ApplicationDbContext>(
+    options => options.UseSqlServer(connectionString, options => options.EnableRetryOnFailure()));
+
+builder.Services.AddAuthorizationBuilder()
+    .AddPolicy("Admin", policy => policy.RequireClaim("Admin"));
+
+builder.Services.AddIdentityApiEndpoints<ApplicationUser>()
+        .AddEntityFrameworkStores<ApplicationDbContext>();
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
@@ -27,24 +43,3 @@ app.MapFallbackToFile("/index.html");
 app.MapIdentityApi<ApplicationUser>();
 
 app.Run();
-
-static void RegisterServices(WebApplicationBuilder builder)
-{
-    var keyVault = builder.Configuration.GetValue<string>("KeyVault") ?? throw new InvalidOperationException();
-    var managedIdentityClientId = builder.Configuration.GetValue<string>("ManagedIdentityClientId") ?? throw new InvalidOperationException();
-    var client = new SecretClient(new Uri(keyVault), new DefaultAzureCredential(new DefaultAzureCredentialOptions { ManagedIdentityClientId = managedIdentityClientId }));
-    var connectionString = client.GetSecret("ApplicationDbContextConnection").Value.Value ?? throw new InvalidOperationException();
-
-    builder.Services.AddDbContext<ApplicationDbContext>(
-        options => options.UseSqlServer(connectionString, options => options.EnableRetryOnFailure()));
-
-    builder.Services.AddAuthorizationBuilder()
-        .AddPolicy("Admin", policy => policy.RequireClaim("Admin"));
-
-    builder.Services.AddIdentityApiEndpoints<ApplicationUser>()
-            .AddEntityFrameworkStores<ApplicationDbContext>();
-
-    builder.Services.AddControllers();
-    builder.Services.AddEndpointsApiExplorer();
-    builder.Services.AddSwaggerGen();
-}
